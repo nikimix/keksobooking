ba163bc62a7b0d0cd913e0a9fb8b5c5ed865e27a
hik <nikitagorbachev1997@gmail.com>
2021-11-24 00:05:51 +0300

    added two functions wich generate random integer number and random float number

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-function getSum(operand1, operand2) {
-  const description = 'Сумма равна';
-  const sum = operand1 + operand2;
-  return `${description  } ${  sum}`;
-}
+const getRandomIntNumber = function (minValue, maxValue) {
+  minValue = Math.ceil(Math.min(Math.abs(minValue)));
+  maxValue = Math.floor(Math.max(Math.abs(maxValue)));
+  const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+  return randomNumber;
+};
+getRandomIntNumber(0,30);
 
-getSum(1, 2);
+const getRandomFloatNumber = function (minValue, maxValue, digits = 1) {
+  minValue = Math.ceil(Math.min(Math.abs(minValue)));
+  maxValue = Math.floor(Math.max(Math.abs(maxValue)));
+  const randomNumber = Math.random() * (maxValue - minValue) + minValue;
+  return randomNumber.toFixed(digits);
+};
+getRandomFloatNumber (0,30,3);
