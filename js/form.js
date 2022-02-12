02ef7ab7e92a29e7a1023936625b04c8f9c255a5
hik <nikitagorbachev1997@gmail.com>
2022-02-12 15:38:00 +0300

    ads filtering logic changed

--- js/form.js
+++ js/form.js
@@ -17,75 +17,74 @@ const setFieldsValueDefault = () => {
   document.querySelector('.ad-form-header__preview').firstElementChild.src = 'img/muffin-grey.svg';
 };
 
-const checkAd = (ad) => {
-  // попробовать написать отдельную универсальную функцию которая возвращает результат проверки. что бы не городить if
-  let truth = true;
-  const housingTypeInput = document.querySelector('#housing-type');
-  const housingPriceInput = document.querySelector('#housing-price');
-  const housingRoomsInput = document.querySelector('#housing-rooms');
-  const housingGuestsInput = document.querySelector('#housing-guests');
-  const housingFeatures = document.querySelectorAll('.map__checkbox');
-  const getPriceValue = () => {
-    if(ad.offer.price <= LOW_PRICE) {
-      return 'low';
-    }
-    else if(ad.offer.price >= LOW_PRICE && ad.offer.price <= HIGH_PRICE){
-      return 'middle';
-    }
+function getStringPriceValue(number) {
+  if(number <= LOW_PRICE) {
+    return 'low';
+  }
+  else if(number >= HIGH_PRICE) {
     return 'high';
-  };
+  }
+  return 'middle';
+}
 
-  if(truth){
-    truth = !!((housingTypeInput.value !== 'any' && housingTypeInput.value === ad.offer.type));
-    if(housingTypeInput.value === 'any') {
-      truth = true;
+function getCurrentFiltersValues() {
+  const type = document.querySelector('#housing-type').value;
+  const price = document.querySelector('#housing-price').value;
+  const roomOfNumber = document.querySelector('#housing-rooms').value;
+  const guestOfNumbers = document.querySelector('#housing-guests').value;
+  const features = [];
+  const housingFeatures = document.querySelectorAll('.map__checkbox');
+
+  housingFeatures.forEach((feature) => {
+    if(feature.checked) {
+      features.push(feature.value);
+    }
+  });
+
+  if(features.length === 0) {
+    features.push('any');
+  } else {
+    features.sort((firstFeature, secondFeature) => (firstFeature > secondFeature) ? 1 : -1);
+  }
+
+  return [type, price, roomOfNumber, guestOfNumbers, features.join('')];
+}
+
+function getStringAdValuesForValidate(ad) {
+  const housingType = ad.offer.type;
+  const housingPrice = getStringPriceValue(ad.offer.price);
+  const numberOfRooms = String(ad.offer.rooms);
+  const numberOfGuests = String(ad.offer.guests);
+  const housingFeatures = [];
+
+  if(ad.offer.features) {
+    ad.offer.features.forEach((item) => housingFeatures.push(item));
+    housingFeatures.sort((firstFeature, secondFeature) => (firstFeature > secondFeature) ? 1 : -1);
+  } else {
+    housingFeatures.push('');
+  }
+
+  return [housingType, housingPrice, numberOfRooms, numberOfGuests, housingFeatures.join('')];
+}
+
+function checkMatchesAdToFilters(adValues, filterValues) {
+  for(let index = 0; index < filterValues.length; index++) {
+    if(filterValues[index] === 'any') {
+      continue;
+    }
+
+    if(filterValues[index] !== adValues[index]) {
+      return false;
     }
   }
-  if(truth) {
-    truth = !!((housingPriceInput.value !== 'any' && housingPriceInput.value === getPriceValue()));
-    if(housingPriceInput.value === 'any') {
-      truth = true;
-    }
-  }
-  if(truth) {
-    truth = !!((housingRoomsInput.value !== 'any' && parseInt(housingRoomsInput.value, 10) === ad.offer.rooms));
-    if(housingRoomsInput.value === 'any') {
-      truth = true;
-    }
-  }
-  if(truth) {
-    truth = !!((housingGuestsInput.value !== 'any' && parseInt(housingGuestsInput.value,10) === ad.offer.guests));
-    if(housingGuestsInput.value === 'any') {
-      truth = true;
-    }
-  }
-  if(truth) {
-    const features = [];
-    for (const item of housingFeatures) {
-      if(item.checked) {
-        features.push(item.value);
-      }
-    }
-    if(features.length !== 0) {
-      if(ad.offer.features) {
-        features.forEach((item) => {
-          if(!ad.offer.features.includes(item)) {
-            truth = false;
-          }
-        });
-      } else {
-        return false;
-      }
-    }
-    return truth;
-  }
-  return truth;
-};
+  return true;
+}
 
 const onChangeMapFilter = (data) => {
+  const currentFilterValues = getCurrentFiltersValues();
   const filteredData = [];
   data.forEach((item) => {
-    if(checkAd(item)) {
+    if(checkMatchesAdToFilters(getStringAdValuesForValidate(item), currentFilterValues)) {
       filteredData.push(item);
     }
   });
